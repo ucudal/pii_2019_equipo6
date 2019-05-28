8f6bbf7b5f61827a34538a1eddfcc94488a3264c
rodromon <rtorres_1999@hotmail.com>
2019-05-28 20:58:34 -0300

    DB creada y algunos avances siguiendo el tutorial

---
+++ Proyecto/Migrations/20190528225237_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RazorPagesMovie.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Project",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Description = table.Column<string>(nullable: true),
+                    RequiredSpecializations = table.Column<string>(nullable: true),
+                    AssignedTechnicians = table.Column<string>(nullable: true),
+                    PostulatedTechnicians = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Project", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Project");
+        }
+    }
+}
