cd81d7cc9881a8d3464cb78787ff1a474677cf4d
rodrigo-silva28 <rodrigo-silva28@users.noreply.github.com>
2019-06-28 20:30:50 -0300

    Agrego clase client
    Lo que dice el titulo

---
+++ Proyecto/src/Model/Client.cs
@@ -0,0 +1,12 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+
+namespace RazorPagesMovie.Models
+{
+    public class Client : Person
+    {
+        //Conocer Proyectos
+    }
+}
